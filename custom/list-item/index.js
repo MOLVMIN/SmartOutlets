c8e0a3fe2516d579f02b14964137a609a851c843
MOLVMIN <506644585@qq.com>
2018-11-27 20:31:07 +0800

    Initial Commit

---
+++ custom/list-item/index.js
@@ -0,0 +1,21 @@
+// custom/list-item/index.js
+Component({
+  externalClasses: ['m-class'],
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    thumb: {
+      type: String,
+      value: ''
+    },
+    title: {
+      type: String,
+      value: ''
+    },
+    date: {
+      type: String,
+      value: ''
+    }
+  }
+})
